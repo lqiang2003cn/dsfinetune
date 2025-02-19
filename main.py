510c9ddf8b52d5eb90406a231185f9ba8c63568e
lqiang2003cn <randycoq@163.com>
2025-02-19 09:03:16 +0000

    0

---
+++ main.py
@@ -0,0 +1,16 @@
+from unsloth import FastLanguageModel
+
+max_seq_length = 2048
+dtype = None
+load_in_4bit = True
+hf_token='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'
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "unsloth/DeepSeek-R1-Distill-Llama-8B",
+    max_seq_length = max_seq_length,
+    dtype = dtype,
+    load_in_4bit = load_in_4bit,
+    token = hf_token,
+)
+
+print('ok')
